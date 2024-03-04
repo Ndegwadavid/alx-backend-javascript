d167020656c7d3f4e3d2ef7366e657746d2a4ca7
david Njoroge <davidndegwa6869@gmail.com>
2024-03-04 10:46:33 +0300

    backend development: Push

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,11 @@
+// function taskFirst to instantiate varables using const
+// fucntion taskNext to instatiate varables using let
+
+export const taskFirst = () => {
+    var task = 'I prefer const when i can';
+    return task;
+}
+export const taskNext = () => {
+    let task = 'But let when i have to';
+    return task;
+}
