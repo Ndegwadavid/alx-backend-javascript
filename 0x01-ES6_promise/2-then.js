fe8a881da112bfac7f7a0ec74bf2c1b5590c04f6
Ndegwadavid <davidndegwa6869@gmail.com>
2024-05-27 07:36:41 +0300

    Code updated

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,5 @@
-function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => {
-      console.log("Got a response from the API");
-      return {
-        status: 200,
-        body: "success",
-      };
-    })
-    .catch(() => {
-      console.log("Got a response from the API");
-      return new Error();
-    });
+export default function handleResponseFromAPI(promise) {
+  promise.then(() => {
+    console.log('Got a response from the API');
+  }, (err) => err);
 }
